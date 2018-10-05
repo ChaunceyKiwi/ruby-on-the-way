e863210700865cff378fa1d57aaf61c9a947ea6b
ChaunceyKiwi <chaunceykiwi@gmail.com>
2018-10-05 15:56:39 -0700

    Remove code that cannot run

--- RSpec/spec/metadata.rb
+++ RSpec/spec/metadata.rb
@@ -9,14 +9,3 @@ RSpec.describe "An Example Group with a metadata variable", :foo => 17 do
     end
   end
 end
-
-RSpec.describe "An Example Group with a metadata variable", :foo => 17 do
-  context "and a context with another variable", :bar => 12 do
-    it "can access the metadata variable in the context block" do |example|
-      expect(example.metadata[:foo]).to eq(17)
-      expect(example.metadata[:bar]).to eq(12)
-      example.metadata.each do |k,v|
-      puts "#{k}: #{v}"
-    end
-  end
-end
